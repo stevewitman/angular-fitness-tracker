8d7937f2d44a075ad6a1e07847f940ebcb98491a
stevewitman <stevewitman@gmail.com>
2018-07-27 23:28:16 -0600

    Style the sidenav and toolbar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,7 +7,8 @@ import {
   MatIconModule,
   MatInputModule,
   MatNativeDateModule,
-  MatSidenavModule
+  MatSidenavModule,
+  MatToolbarModule
  } from '@angular/material';
 
 @NgModule({
@@ -19,7 +20,8 @@ import {
     MatIconModule,
     MatInputModule,
     MatNativeDateModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule
   ],
   exports: [
     MatButtonModule,
@@ -29,7 +31,8 @@ import {
     MatIconModule,
     MatInputModule,
     MatNativeDateModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule {}
