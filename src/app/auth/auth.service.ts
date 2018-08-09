72c5a7756904af8b4758b1955728f8d72572acd7
stevewitman <stevewitman@gmail.com>
2018-08-08 19:03:47 -0600

    Add routing to authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,11 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subject } from 'rxjs/Subject';
 
 import { User } from './user.model';
 import { AuthData } from './auth-data.model';
 
+@Injectable()
 export class AuthService {
   authChange = new Subject<boolean>();
   private user: User; // initially undefined
+
+  constructor(private router: Router) {}
 
   registerUser(authData: AuthData) {
     this.user = {
@@ -13,6 +18,7 @@ export class AuthService {
       userId: Math.round(Math.random() * 10000).toString()
     };
     this.authChange.next(true);
+    this.router.navigate(['/training']);
   }
 
   login(authData: AuthData) {
@@ -21,11 +27,13 @@ export class AuthService {
       userId: Math.round(Math.random() * 10000).toString()
     };
     this.authChange.next(true);
+    this.router.navigate(['/training']);
   }
 
   logout() {
     this.user = null;
     this.authChange.next(false);
+    this.router.navigate(['/login']);
   }
 
   getUser() {
